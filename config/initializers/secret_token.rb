3fca8e4827e9161d54689d50968207d9081ad551
magran17 <mmm.bmw@gmail.com>
2013-07-13 00:41:51 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2b::Application.config.secret_token = '3d0df8cb90fdb5ecd54b5da8b94cdd064ab9b2c1a67b7a9d07d0419acb73e7d5a4297aa305c262698e128c96d7b986eae1ba0e0e17d547c977d54b2e76f652b8'
